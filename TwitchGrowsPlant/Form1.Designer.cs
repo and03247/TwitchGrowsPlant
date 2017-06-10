22ba80581336ac93e5bb3362e33df5f0bee4cc89
Elliot Anderson <and03247@umn.edu>
2017-06-09 22:24:18 -0500

    Initial Master Commit
    Initial Commit

---
+++ TwitchGrowsPlant/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace TwitchGrowsPlant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ChatBox = new System.Windows.Forms.RichTextBox();
+            this.GuessBox = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel1.BackgroundImage")));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.36364F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.36364F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
+            this.tableLayoutPanel1.Controls.Add(this.ChatBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.GuessBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(981, 504);
+            this.tableLayoutPanel1.TabIndex = 3;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.AccessibleName = "ChatBox";
+            this.ChatBox.Location = new System.Drawing.Point(3, 3);
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(350, 387);
+            this.ChatBox.TabIndex = 0;
+            this.ChatBox.Text = "";
+            // 
+            // GuessBox
+            // 
+            this.GuessBox.Location = new System.Drawing.Point(359, 3);
+            this.GuessBox.Name = "GuessBox";
+            this.GuessBox.Size = new System.Drawing.Size(350, 387);
+            this.GuessBox.TabIndex = 3;
+            this.GuessBox.Text = "";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(359, 396);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(350, 100);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 50);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(3, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(344, 50);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Change This One";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 396);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(350, 100);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(344, 50);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(3, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 50);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Guess This One";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(715, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(263, 387);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(737, 423);
+            this.button1.Margin = new System.Windows.Forms.Padding(25, 30, 25, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 51);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Reconnect";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1006, 529);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox ChatBox;
+        private System.Windows.Forms.RichTextBox GuessBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TwitchGrowsPlant/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Media;
+using System.Diagnostics;
+using System.Timers;
+
+namespace TwitchGrowsPlant
+{
+    public partial class Form1 : Form
+    {
+        #region Globals
+        private static string UserName = "twitch_grows_a_plant";
+        private static string Password = "oauth:gp4eotvzywddewgk2xysjiihlgicaj";
+        private IrcClient irc = new IrcClient("irc.chat.twitch.tv", 6667, UserName, Password);
+        NetworkStream ServerStream = default(NetworkStream);
+        string ReadData = "";
+        Thread ChatThread;
+
+        // Timer
+        private System.Timers.Timer pingTimer;
+
+        // Color Control
+        int lR = 3;
+        int lG = 2;
+        int lB = 2;
+
+        int rR = 0;
+        int rG = 0;
+        int rB = 0;
+
+        public static Random random = new Random();
+
+        // Difficulty
+        public static int multiple = 30;
+        private static int lenient = 31; //leniency should probably be a multiple of the multiplier
+                                         // in-order to make any difference.
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label2.Text = "???";
+            NotifyGuessChange(); // Set label and stuff
+            RandomizeChatColor(); // Set Random color AND label and stuff
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            irc.JoinRoom("e018s");
+            ChatThread = new Thread(GetMessage);
+            ChatThread.Start();
+
+            // Initialize Timers
+            pingTimer = new System.Timers.Timer();
+            pingTimer.Elapsed += (pingTimer, ea) => PingTimeEvent(sender, ea, this);
+            pingTimer.Interval = 5000;
+            pingTimer.Enabled = true;
+
+        }
+
+        private void GetMessage()
+        {
+            ServerStream = irc.tcpClient.GetStream();
+            int buffsize = 0;
+            byte[] inStream = new byte[10025];
+            buffsize = irc.tcpClient.ReceiveBufferSize;
+
+            while(true)
+            {
+                try
+                {
+                    ReadData = irc.ReadMessage();
+                    msg();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+
+        private void msg() // THIS is WHERE EVERYTHING IS DEALT WITH IN CHAT
+                           // Commands, automatic timeouts, etc.
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(msg));
+                }
+                else
+                {
+                    //ChatBox.Text = ChatBox.Text + ReadData.ToString() + Environment.NewLine;
+                    Debug.WriteLine(ReadData);
+                    if (ReadData.Contains("PRIVMSG"))
+                    {
+                        string[] msgseparator = new string[] { "#e018s :" }; // Room name
+                        string user = ReadData.Split('!')[0];
+                        user = user.Substring(1); // get rid of the starging colon
+                        string message = ReadData.Split(msgseparator, StringSplitOptions.None)[1];
+                        //ChatBox.Text = ChatBox.Text + user + ": " + message + "\n";
+
+                        // Colors
+                        // Red
+                        if (message.ToLower().Equals("r+") || message.ToLower().Equals("red+"))
+                        {
+                            IncrementRed();
+                        }
+                        if (message.ToLower().Equals("r-") || message.ToLower().Equals("red-"))
+                        {
+                            DecrementRed();
+                        }
+                        // Green
+                        if (message.ToLower().Equals("g+") || message.ToLower().Equals("green+"))
+                        {
+                            IncrementGreen();
+                        }
+                        if (message.ToLower().Equals("g-") || message.ToLower().Equals("green-"))
+                        {
+                            DecrementGreen();
+                        }
+                        // Blue
+                        if (message.ToLower().Equals("b+") || message.ToLower().Equals("blue+"))
+                        {
+                            IncrementBlue();
+                        }
+                        if (message.ToLower().Equals("b-") || message.ToLower().Equals("blue-"))
+                        {
+                            DecrementBlue();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        #region ColorControl
+        // Red
+        private void IncrementRed()
+        {
+            if (rR < 255 - multiple)
+            {
+                rR = rR + multiple;
+                NotifyGuessChange();
+            }
+        }
+
+        private void DecrementRed()
+        {
+            if (rR >= 0 + multiple)
+            {
+                rR = rR - multiple;
+                NotifyGuessChange();
+            }
+        }
+        // Green
+        private void IncrementGreen()
+        {
+            if (rG < 255 - multiple)
+            {
+                rG = rG + multiple;
+                NotifyGuessChange();
+            }
+        }
+
+        private void DecrementGreen()
+        {
+            if (rG >= 0 + multiple)
+            {
+                rG = rG - multiple;
+                NotifyGuessChange();
+            }
+        }
+        // Blue
+        private void IncrementBlue()
+        {
+            if (rB < 255 - multiple)
+            {
+                rB = rB + multiple;
+                NotifyGuessChange();
+            }
+        }
+
+        private void DecrementBlue()
+        {
+            if (rB >= 0 + multiple)
+            {
+                rB = rB - multiple;
+                NotifyGuessChange();
+            }
+        }
+
+
+
+        // ETC
+        
+        private void Reconnect()
+        {
+            irc = new IrcClient("irc.chat.twitch.tv", 6667, UserName, Password);
+            irc.JoinRoom("e018s");
+        }
+       
+        private void NotifyGuessChange()
+        {
+            GuessBox.BackColor = Color.FromArgb(rR, rG, rB); // Initial guessbox color
+            label1.Text = "R(" + rR + "), G(" + rG + "), B(" + rB + ")";
+
+            // Check Guess
+            if (InRange())
+            {
+                RandomizeChatColor();
+            }
+        }
+
+        private void NotifyChatChange()
+        {
+            ChatBox.BackColor = Color.FromArgb(lR, lG, lB); // Initial guessbox color
+            //label2.Text = "R(" + lR + "), G(" + lG + "), B(" + lB + ")";
+        }
+
+        private bool InRange()
+        {
+            bool winner = true;
+            // Check red
+            if (!(rR>=lR-lenient && rR<=lR+lenient)) // if not in lenient range
+            {
+                winner = false;
+            }
+            // Check green
+            if (!(rG >= lG - lenient && rG <= lG + lenient)) // if not in lenient range
+            {
+                winner = false;
+            }
+            // Check blue
+            if (!(rB >= lB - lenient && rB <= lB + lenient)) // if not in lenient range
+            {
+                winner = false;
+            }
+
+            return winner;
+        }
+
+        private void RandomizeChatColor()
+        {
+            // Generates a random number between 0 and 255 of step size MULTIPLE
+            lR = random.Next(0, (255 / multiple) + 1) * multiple;
+            lG = random.Next(0, (255 / multiple) + 1) * multiple;
+            lB = random.Next(0, (255 / multiple) + 1) * multiple;
+
+            irc.SendChatMessage("Congradulations, you got the color. New Color Generated");
+
+            NotifyChatChange();
+        }
+
+        #endregion
+
+        #region TimerEvents
+
+        private static void PingTimeEvent(object source, ElapsedEventArgs e, Form1 form)
+        {
+            form.irc.PingResponse();
+            // Debug
+            //form.irc.SendChatMessage("Pinged Ya! " + new Random().Next(0,1000));
+            //
+        }
+
+        #endregion
+
+        #region OnClicks
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            irc.SendChatMessage("Hey Buddy Boy (Sent From My Windows Form App)");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            irc.LeaveRoom();
+            ServerStream.Dispose();
+            Environment.Exit(0);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Reconnect();
+            irc.SendChatMessage("I Clicked the button !");
+        }
+    }
+
+    public class IrcClient
+    {
+        private string username;
+        private string channel;
+
+        public TcpClient tcpClient;
+        private StreamReader inputStream;
+        private StreamWriter outputStream;
+
+        // Constructor
+        public IrcClient(string ip, int port, string username, string password)
+        {
+            tcpClient = new TcpClient(ip, port);
+            inputStream = new StreamReader(tcpClient.GetStream());
+            outputStream = new StreamWriter(tcpClient.GetStream());
+
+            outputStream.WriteLine("PASS " + password);
+            outputStream.WriteLine("Nick " + username);
+            outputStream.WriteLine("USER " + username + " 8 * :" + username);
+            outputStream.WriteLine("CAP REQ :twitch.tv/membership");
+            outputStream.WriteLine("CAP REQ :twitch.tv/commands");
+            outputStream.Flush();
+        }
+
+        public void JoinRoom(string channel)
+        {
+            this.channel = channel;
+            outputStream.WriteLine("JOIN #" + channel);
+            outputStream.Flush();
+        }
+
+        public void LeaveRoom()
+        {
+            outputStream.Close();
+            inputStream.Close();
+        }
+
+        public void SendIrcMessage(string message)
+        {
+            outputStream.WriteLine(message);
+            outputStream.Flush();
+        }
+
+        public void SendChatMessage(string message)
+        {
+            SendIrcMessage(":" + username + "!" + username + "@" + username 
+                + ".tmi.twitch.tv PRIVMSG #" + channel + " :" + message);
+        }
+
+        public void PingResponse()
+        {
+            SendIrcMessage("PONG tmi.twitch.tv\r\n");
+        }
+
+        public string ReadMessage()
+        {
+            string message = "";
+            message = inputStream.ReadLine();
+            return message;
+        }
+    }
+}
