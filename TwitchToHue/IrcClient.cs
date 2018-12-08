5bf25530bc360d2623b4743259a05c6e23e6af8b
Reilly Hallstrom <rhallstrom@saturnsys.com>
2018-12-08 10:54:10 -0600

    Added new .net core project to solution and separated out the ircclient to its own class. Also moved the username, password, etc to an app.config.

---
+++ TwitchToHue/IrcClient.cs
@@ -0,0 +1,65 @@
+﻿namespace TwitchToHue
+{
+    using System.IO;
+    using System.Net.Sockets;
+    
+    public class IrcClient
+    {
+        private readonly string username;
+        private string channel;
+        public TcpClient TcpClient;
+        private readonly StreamReader inputStream;
+        private readonly StreamWriter outputStream;
+
+        // Constructor
+        public IrcClient(string ip, int port, string username, string password)
+        {
+            this.username = username;
+            this.TcpClient = new TcpClient(ip, port);
+            inputStream = new StreamReader(this.TcpClient.GetStream());
+            outputStream = new StreamWriter(this.TcpClient.GetStream());
+
+            outputStream.WriteLine("PASS " + password);
+            outputStream.WriteLine("Nick " + username);
+            outputStream.WriteLine("USER " + username + " 8 * :" + username);
+            outputStream.WriteLine("CAP REQ :twitch.tv/membership");
+            outputStream.WriteLine("CAP REQ :twitch.tv/commands");
+            outputStream.Flush();
+        }
+
+        public void JoinRoom(string channelName)
+        {
+            channel = channelName;
+            outputStream.WriteLine("JOIN #" + channelName);
+            outputStream.Flush();
+        }
+
+        public void LeaveRoom()
+        {
+            outputStream.Close();
+            inputStream.Close();
+        }
+
+        public void SendIrcMessage(string message)
+        {
+            outputStream.WriteLine(message);
+            outputStream.Flush();
+        }
+
+        public void SendChatMessage(string message)
+        {
+            SendIrcMessage(":" + username + "!" + username + "@" + username
+                            + ".tmi.twitch.tv PRIVMSG #" + channel + " :" + message);
+        }
+
+        public void PingResponse()
+        {
+            SendIrcMessage("PONG tmi.twitch.tv\r\n");
+        }
+
+        public string ReadMessage()
+        {
+            return inputStream.ReadLine();
+        }
+    }
+}
---
+++ TwitchToHue/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace TwitchToHue
+{
+    using System.Configuration;
+    using System.Net.Sockets;
+    using System.Threading;
+
+    class Program
+    {
+        #region Globals
+        private readonly string userName = ConfigurationManager.AppSettings["username"];
+        private readonly string password = ConfigurationManager.AppSettings["password"];
+        private readonly string ip = ConfigurationManager.AppSettings["ip"];
+        private readonly string port = ConfigurationManager.AppSettings["port"];
+        private readonly string room = ConfigurationManager.AppSettings["room"];
+        private IrcClient irc;
+        private string readData = "";
+        private Thread chatThread;
+        private NetworkStream serverStream = default(NetworkStream);
+
+        // Timer
+        private System.Timers.Timer pingTimer;
+
+        #endregion
+
+        private static void Main(string[] args)
+        {
+            var program = new Program();
+            program.OnStartup();
+        }
+
+        private void OnStartup()
+        {
+            irc = new IrcClient(ip, int.Parse(port), userName, password);
+
+            irc.JoinRoom(room);
+            chatThread = new Thread(GetMessage);
+            chatThread.Start();
+
+            // Initialize Timers
+            pingTimer = new System.Timers.Timer();
+            pingTimer.Elapsed += (pingTimer, ea) => PingTimeEvent(this, ea, this);
+            pingTimer.Interval = 5000;
+            pingTimer.Enabled = true;
+        }
+
+        private void Msg() // This is where everything is dealt with in chat: commands, automatic timeouts, etc.
+        {
+            try
+            {
+                Debug.WriteLine(readData);
+                Console.WriteLine(readData);
+                if (readData.Contains("PRIVMSG"))
+                {
+                    var msgseparator = new string[] { "#" + this.room + " :" };
+                    var user = readData.Split('!')[0];
+                    user = user.Substring(1); // get rid of the starting colon
+                    var message = readData.Split(msgseparator, StringSplitOptions.None)[1];
+
+                    // TODO: Do something with the message here
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        private void GetMessage()
+        {
+            serverStream = irc.TcpClient.GetStream();
+
+            while (true)
+            {
+                try
+                {
+                    readData = irc.ReadMessage();
+                    Msg();
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }
+        }
+
+        #region TimerEvents
+
+        private static void PingTimeEvent(object source, ElapsedEventArgs e, Program program)
+        {
+            program.irc.PingResponse();
+        }
+
+        #endregion
+
+    }
+}
